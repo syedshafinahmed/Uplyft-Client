6047788daab880d5848730d13c0c3bbd5e3188ec
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-10 14:31:59 +0600

    added password validation

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -8,8 +8,20 @@ const Register = () => {
   const { createUser, updateUserProfile } = use(AuthContext);
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
+
+  const handlePasswordChange = (event) => {
+    const password = event.target.value;
+    if (!/(?=.*[a-z])/.test(password) || !/(?=.*[A-Z])/.test(password) || password.length < 6) {
+      setPasswordError(
+        'Password must have at least 1 uppercase, 1 lowercase, and be 6+ characters long.'
+      );
+    } else {
+      setPasswordError('');
+    }
+  }
 
   const handleRegister = (event) => {
     event.preventDefault();
@@ -17,6 +29,7 @@ const Register = () => {
     const photoURL = event.target.photoURL.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
+    if (passwordError) return;
     setLoading(true);
     createUser(email, password)
       .then((result) => {
@@ -62,7 +75,7 @@ const Register = () => {
               <input type="text" name="photoURL" className="input rounded-full focus:border-0 focus:outline-indigo-800" placeholder="Photo URL" />
               <label className="label">Password</label>
               <div className="relative">
-                <input type={showPassword ? "text" : "password"} className="input rounded-full focus:border-0 focus:outline-indigo-800" name="password" placeholder="Password" />
+                <input type={showPassword ? "text" : "password"} className="input rounded-full focus:border-0 focus:outline-indigo-800" name="password" onChange={handlePasswordChange} placeholder="Password" />
                 <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute z-10 right-8 top-1/2 -translate-y-1/2 text-indigo-800">
                   {showPassword ? (
                     <PiEyeSlashDuotone size={15} />
@@ -73,6 +86,11 @@ const Register = () => {
               </div>
 
               <button className="btn rounded-full text-base bg-linear-to-b from-indigo-800 to-violet-500 text-white mt-3">Register</button>
+              {passwordError && (
+                <p className="text-red-600 text-sm mt-2 text-center font-medium">
+                  {passwordError}
+                </p>
+              )}
               <p className='mt-2 text-center'>Already have an account? <NavLink to='/login' className='text-indigo-800 font-bold'>Login</NavLink></p>
             </fieldset>
           </form>
